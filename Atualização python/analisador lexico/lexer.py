8d99f1fecc8e98fe86d98c1e19ce5e73882d8e03
Júlia Saldanha <juliasvilar@gmail.com>
2024-06-10 19:19:41 -0300

    FINAL

--- Atualização python/analisador lexico/lexer.py
+++ Atualização python/analisador lexico/lexer.py
@@ -1,30 +1,19 @@
 import ply.lex as lex
 
-# Definindo os tokens
 tokens = (
-    'INT',
-    'FLOAT',
-    'DOUBLE',
-    'CHAR',
-    'BOOLEAN',
-    'ID',
-    'NUM_INT',
-    'NUM_DEC',
-    'TEXTO',
-    'CONDICIONAL',
-    'FUNCAO_RECURSIVA',
-    'RETORNO',
-    'FUNCAO_PRINCIPAL',
-    'PALAVRA_RES',
-    'BIBLIOTECA',
-    'OPERADOR',
-    'SIMBOLO_ESP'
+    'INT', 'FLOAT', 'DOUBLE', 'CHAR', 'BOOLEAN',
+    'ID', 'NUM_INT', 'NUM_DEC', 'TEXTO',
+    'CONDICIONAL', 'FUNCAO_RECURSIVA', 'RETORNO', 'FUNCAO_PRINCIPAL',
+    'PALAVRA_RES', 'BIBLIOTECA', 'OPERADOR', 'SIMBOLO_ESP',
+    'VOID', 'IF', 'ELSE', 'WHILE', 'FOR', 'RETURN', 'PRINTF',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
+    'ASSIGN', 'ADD_ASSIGN', 'SUB_ASSIGN', 'MUL_ASSIGN', 'DIV_ASSIGN', 'MOD_ASSIGN',
+    'GE', 'LE', 'GT', 'LT', 'EQ', 'NE',
+    'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'LBRACKET', 'RBRACKET', 'SEMICOLON', 'COMMA'
 )
 
-# Ignorar espaços em branco e tabulações
 t_ignore = ' \t'
 
-# Definindo expressões regulares para tokens
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value, 'ID')
@@ -46,7 +35,7 @@ def t_TEXTO(t):
 
 def t_COMENTARIO(t):
     r'\/\/.*'
-    pass  # Comentários são ignorados
+    pass
 
 def t_newline(t):
     r'\n+'
@@ -56,11 +45,10 @@ def t_error(t):
     print("Erro: Caractere não reconhecido '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Palavras reservadas
 reserved = {
-    'void': 'PALAVRA_RES',
+    'void': 'VOID',
     'scanf': 'PALAVRA_RES',
-    'printf': 'PALAVRA_RES',
+    'printf': 'PRINTF',
     'stdio': 'BIBLIOTECA',
     'string': 'BIBLIOTECA',
     'stdlib': 'BIBLIOTECA',
@@ -69,22 +57,48 @@ reserved = {
     'double': 'DOUBLE',
     'char': 'CHAR',
     'boolean': 'BOOLEAN',
-    'if': 'CONDICIONAL',
-    'else': 'CONDICIONAL',
-    'for': 'FUNCAO_RECURSIVA',
-    'while': 'FUNCAO_RECURSIVA',
-    'return': 'RETORNO',
+    'if': 'IF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'return': 'RETURN',
     'main': 'FUNCAO_PRINCIPAL'
 }
-
-# Definindo tokens para operadores e símbolos especiais
+s
 t_OPERADOR = r'[\+\-\*/=<>!&|]'
 t_SIMBOLO_ESP = r'[\(\){};,\[\]]'
 
-# Construindo o lexer
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MOD = r'%'
+
+t_ASSIGN = r'='
+t_ADD_ASSIGN = r'\+='
+t_SUB_ASSIGN = r'-='
+t_MUL_ASSIGN = r'\*='
+t_DIV_ASSIGN = r'/='
+t_MOD_ASSIGN = r'%='
+
+t_GE = r'>='
+t_LE = r'<='
+t_GT = r'>'
+t_LT = r'<'
+t_EQ = r'=='
+t_NE = r'!='
+
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_SEMICOLON = r';'
+t_COMMA = r','
+
 lexer = lex.lex()
 
-# Código de teste
 data = '''
 int main() {
     printf("Hello, world!");
@@ -92,10 +106,8 @@ int main() {
 }
 '''
 
-# Alimente o lexer com o código de teste
 lexer.input(data)
 
-# Tokenize e imprima os tokens
 while True:
     tok = lexer.token()
     if not tok:
