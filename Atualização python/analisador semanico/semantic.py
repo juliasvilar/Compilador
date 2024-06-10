de6b716f993f95cbbf3f9a50fbf44a2f25f8b47d
Isabela Medeiros <135226718+belamedeirosbl@users.noreply.github.com>
2024-06-10 10:48:01 -0300

    Update semantic.py

--- Atualização python/analisador semanico/semantic.py
+++ Atualização python/analisador semanico/semantic.py
@@ -1,27 +1,70 @@
-variaveis = {}
-num_variaveis = 0
+class Variavel:
+    def __init__(self, nome, tipo):
+        self.nome = nome
+        self.tipo = tipo
 
-funcoes = {}
-num_funcoes = 0
+variaveis = []
+
+class Funcao:
+    def __init__(self, nome, tipo_retorno, tipos_parametros):
+        self.nome = nome
+        self.tipo_retorno = tipo_retorno
+        self.tipos_parametros = tipos_parametros
+
+funcoes = []
 
 def adicionarVariavel(nome, tipo):
-    global variaveis, num_variaveis
-    variaveis[nome] = tipo
-    num_variaveis += 1
+    global variaveis
+    variaveis.append(Variavel(nome, tipo))
 
 def verificarDeclaracaoVariavel(nome):
-    return nome in variaveis
+    for variavel in variaveis:
+        if variavel.nome == nome:
+            return True
+    return False
 
 def verificarCompatibilidadeTipos(tipo1, tipo2):
-    return tipo1 == tipo2
+    tipos_numericos = ["int", "float"]
+
+    # Verificação para operações aritméticas
+    if tipo1 in tipos_numericos and tipo2 in tipos_numericos:
+        return True
+
+    # Verificação para operações lógicas
+    if tipo1 == "boolean" and tipo2 == "boolean":
+        return True
+
+    return False
 
 def obterTipoVariavel(nome):
-    return variaveis.get(nome, None)
+    for variavel in variaveis:
+        if variavel.nome == nome:
+            return variavel.tipo
+    return None
 
 def adicionarFuncao(nome, tipo_retorno, tipos_parametros):
-    global funcoes, num_funcoes
-    funcoes[nome] = {'tipo_retorno': tipo_retorno, 'tipos_parametros': tipos_parametros}
-    num_funcoes += 1
+    global funcoes
+    funcoes.append(Funcao(nome, tipo_retorno, tipos_parametros))
 
 def verificarDeclaracaoFuncao(nome):
-    return nome in funcoes
+    for funcao in funcoes:
+        if funcao.nome == nome:
+            return True
+    return False
+
+def verificarTiposParametros(nome_funcao, tipos_argumentos):
+    for funcao in funcoes:
+        if funcao.nome == nome_funcao:
+            if len(funcao.tipos_parametros) != len(tipos_argumentos):
+                return False
+            for tipo_parametro, tipo_argumento in zip(funcao.tipos_parametros, tipos_argumentos):
+                if tipo_parametro != tipo_argumento:
+                    return False
+            return True
+    return False
+
+def obterTipoRetorno(nome_funcao):
+    for funcao in funcoes:
+        if funcao.nome == nome_funcao:
+            return funcao.tipo_retorno
+    return None
