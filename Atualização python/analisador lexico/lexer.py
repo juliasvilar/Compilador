92b9bb5bc4dbc7c877335f2dba2cff0a53841953
Isabela Medeiros <135226718+belamedeirosbl@users.noreply.github.com>
2024-06-10 21:41:28 -0300

    Update lexer.py

--- Atualização python/analisador lexico/lexer.py
+++ Atualização python/analisador lexico/lexer.py
@@ -1,49 +1,54 @@
 import ply.lex as lex
 
-tokens = (
-    'INT', 'FLOAT', 'DOUBLE', 'CHAR', 'BOOLEAN',
-    'ID', 'NUM_INT', 'NUM_DEC', 'TEXTO',
-    'CONDICIONAL', 'FUNCAO_RECURSIVA', 'RETORNO', 'FUNCAO_PRINCIPAL',
-    'PALAVRA_RES', 'BIBLIOTECA', 'OPERADOR', 'SIMBOLO_ESP',
-    'VOID', 'IF', 'ELSE', 'WHILE', 'FOR', 'RETURN', 'PRINTF',
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
-    'ASSIGN', 'ADD_ASSIGN', 'SUB_ASSIGN', 'MUL_ASSIGN', 'DIV_ASSIGN', 'MOD_ASSIGN',
-    'GE', 'LE', 'GT', 'LT', 'EQ', 'NE',
-    'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'LBRACKET', 'RBRACKET', 'SEMICOLON', 'COMMA'
-)
+tokens = ('INT', 'FLOAT', 'DOUBLE', 'CHAR', 'BOOLEAN', 'ID', 'NUM_INT',
+          'NUM_DEC', 'TEXTO', 'CONDICIONAL', 'FUNCAO_RECURSIVA', 'RETORNO',
+          'FUNCAO_PRINCIPAL', 'PALAVRA_RES', 'BIBLIOTECA', 'OPERADOR',
+          'SIMBOLO_ESP', 'VOID', 'IF', 'ELSE', 'WHILE', 'FOR', 'RETURN',
+          'PRINTF', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD', 'ASSIGN',
+          'ADD_ASSIGN', 'SUB_ASSIGN', 'MUL_ASSIGN', 'DIV_ASSIGN', 'MOD_ASSIGN',
+          'GE', 'LE', 'GT', 'LT', 'EQ', 'NE', 'LPAREN', 'RPAREN', 'LBRACE',
+          'RBRACE', 'LBRACKET', 'RBRACKET', 'SEMICOLON', 'COMMA')
 
 t_ignore = ' \t'
+
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value, 'ID')
     return t
 
+
 def t_NUM_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
 
 def t_NUM_DEC(t):
     r'\d+\.\d+'
     t.value = float(t.value)
     return t
 
+
 def t_TEXTO(t):
     r'\"([^\\\"]|\\.)*\"'
     return t
+
 
 def t_COMENTARIO(t):
     r'\/\/.*'
     pass
 
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
 def t_error(t):
     print("Erro: Caractere não reconhecido '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 reserved = {
     'void': 'VOID',
@@ -98,18 +103,3 @@ t_SEMICOLON = r';'
 t_COMMA = r','
 
 lexer = lex.lex()
-
-data = '''
-int main() {
-    printf("Hello, world!");
-    return 0;
-}
-'''
-
-lexer.input(data)
-
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok)
